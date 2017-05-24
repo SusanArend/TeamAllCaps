47c037fab603563d31b1f322a2f99ad7f13cd485
DESKTOP-68M883D\nerne <northnern@gmail.com>
2017-05-23 21:34:17 -0500

    added stuff to main server.js, /routes/(html&api routes), and an incomplete 'plaudit' db model in /models

--- db/users.js
+++ db/users.js
@@ -1,3 +1,5 @@
+//IMPORTANT TODO - instead of listing an array of objects for test users here, instead use sequelize to grab mysql user data
+
 var records = [
     { id: 1, username: 'jack', password: 'secret', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] }
   , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
