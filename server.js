c7bbd0c450e74a811fd9b08f7efe56a3035792de
yilinxu <yellen10@gmail.com>
2017-05-25 20:36:50 -0500

    add employ_badge table

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static("./public"));
 // require("./routes/author-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
