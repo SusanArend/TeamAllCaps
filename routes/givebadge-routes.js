beb4f3d65600482ed310b4bb63841f45eb3c8481
yilinxu <yellen10@gmail.com>
2017-05-28 13:26:06 -0500

    update givebadge-routes.js

--- routes/givebadge-routes.js
+++ routes/givebadge-routes.js
@@ -2,11 +2,11 @@ var db = require("../models");
 
 module.exports = function(app){
 	// Render landing.html at route "/"
-	app.post("/:badge/post", function(req,res){
+	app.post("/badge/post", function(req,res){
 		db.employ_badge.create({
-			sender_id: req.body.sender,
-			recipient_id: req.body.recipient,
-			req.params.badge: 1,
+			sender_name: req.body.sender,
+			recipient_name: req.body.recipient,
+			badge: req.body.badge,
 			comment: req.body.comment
 		}).then(function(){
 			res.redirect("/display")
