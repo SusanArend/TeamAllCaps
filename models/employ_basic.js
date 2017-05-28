f0bfe850563864a97a8192032e90f5cfa71bf5a9
yilinxu <yellen10@gmail.com>
2017-05-28 16:22:43 -0500

    update employ_basic.js

--- models/employ_basic.js
+++ models/employ_basic.js
@@ -41,7 +41,7 @@ module.exports = function(sequelize, DataTypes) {
       }
     },
     photo_path: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: false
     }
   });
