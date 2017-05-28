c18d7f204d3abf9b7a3a0419fafac6a2ee8c667d
yilinxu <yellen10@gmail.com>
2017-05-28 16:34:20 -0500

    update profile-routes.js

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -2,20 +2,26 @@ var db = require("../models");
 
 module.exports =  function(app){
 	app.get("/profile/:name", function(req, res){
-		var hbsObject = {
-			name: req.params.name,
-			img_url : db.employ_basic.findOne({
-				where: {
-					name: req.params.name
-				}
-			}).then(function(data){
-				return data.photo_path;
-			}),
-			badges: db.employ_badge.findAll({
-				where: {
-					$or: [{sender_name: this.name}, {recipient_name: this.name}]
-				}, order: 'createdAt DESC', limit:10
-			})
+		var valid_names = db.employ_basic.findAll({attributes:name});
+		if(valid_names.indexOf(req.params.name)!==-1){
+			var hbsObject = {
+				name: req.params.name,
+				img_url : db.employ_basic.findOne({
+					where: {
+						name: req.params.name
+					}
+				}).then(function(data){
+					return data.photo_path;
+				}),
+				badges: db.employ_badge.findAll({
+					where: {
+						$or: [{sender_name: this.name}, {recipient_name: this.name}]
+					}, order: 'createdAt DESC', limit:10
+				})
+			}
+		}else{
+			res.send("invalid name");
 		}
+		
 	})
 };
