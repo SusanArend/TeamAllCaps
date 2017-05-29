6a8e4fbd12fdd0f00786cf70a451afb3a20b5bbd
DESKTOP-68M883D\nerne <northnern@gmail.com>
2017-05-28 21:16:00 -0500

    missed a comma

--- routes/landing-routes.js
+++ routes/landing-routes.js
@@ -1,6 +1,6 @@
 var path = require("path");
 var db = require("../models");
-var passport = require('passport');.
+var passport = require('passport');
 
 module.exports = function(app){
 	// Render landing.html at route "/"
