f868a5a7facad86c249593193fb348c926074c94
NorthNern <superspork7@gmail.com>
2017-06-02 08:01:28 -0500

    Update faq-routes.js
    Added authentication (no longer public, requires login)

--- routes/faq-routes.js
+++ routes/faq-routes.js
@@ -1,7 +1,10 @@
 var db = require("../models");
+var authentication = require("../config/authentication.js");
 
 module.exports =  function(app){
-	app.get("/faq", function(req, res){
+	app.get("/faq", 
+	require('connect-ensure-login').ensureLoggedIn(),
+	function(req, res){
 		res.render("faq");
 	})
 }
