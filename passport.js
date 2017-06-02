b12dcdb6ea08a740fe42c73a7362c92bc8fa699a
NorthNern <superspork7@gmail.com>
2017-06-02 13:16:28 -0500

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,7 +1,6 @@
 var passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
     bcrypt = require('bcryptjs'),
-    // EmployBasic = require('./models/employ_basic.js') may not need this at all
     db = require('./models')
 
 module.exports = function(app) {
@@ -9,24 +8,25 @@ module.exports = function(app) {
   app.use(passport.session())
 
 
-
+//Declares passport-local as the login strategy, authentication happens via local server/db checks.
   passport.use(new LocalStrategy(
     function(username, password, done) {
-      // process.nextTick(function() {
-      // console.log("hitting passport")
+      //Finds user in db based on username (which is the email in db)
       db.employ_basic.findOne({ 
         where: {
           email: username
         },
         include:[db.employ_option]
       }).then(function (data) {
+          //Returns error if there is no user when login attempted.
           if (!data) { 
-            console.log("no user")
-            return done(null, false, { message: 'This email is not in internal system' })
+            return done(null, false, { message: 'This email is not in the system.' })
           }
         var user = data.dataValues;
         if(user.employ_option){
             var userpassword = user.employ_option.dataValues.password
+            //Encrypts the password the user entered in login attempt, checks it against the encrypted string stored in database to see if a match.
+            //Original user password is never known/shown for security.
             bcrypt.compare(password,userpassword, function(err, res) {
               if (err)
                   throw err;
@@ -34,11 +34,10 @@ module.exports = function(app) {
                 console.log("logged in")
                 return done(null, user)
               }else{
-                console.log("wrong password")
-                return done(null, false, { message: 'Incorrect credentials. please login again' })
+                return done(null, false, { message: 'Incorrect credentials. please login again.' })
               }
             })
-        }else{return done(null, false, { message: "You didn't register before, please sign up" })}
+        }else{return done(null, false, { message: "This email is not in the system." })}
      
 
         })
@@ -46,11 +45,12 @@ module.exports = function(app) {
   ))
 
 
-
+//Serializes user as their UserID
   passport.serializeUser(function(user, done){
     done(null, user.id)
   });
 
+ //Deserializes user on page load using their UserID to get full user info, which can be used with req.user
   passport.deserializeUser(function(id, done) {
     db.employ_basic.findOne({
       where: {
