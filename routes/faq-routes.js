11dd624fb9ede5710f72c14c845092c74bab5fb3
meeshyd <michellemdidier@gmail.com>
2017-06-02 20:32:33 -0500

    fixed link color, added bottom margin, disabled scroll for mobile

--- routes/faq-routes.js
+++ routes/faq-routes.js
@@ -3,7 +3,7 @@ var authentication = require("../config/authentication.js");
 
 module.exports =  function(app){
 	app.get("/faq", 
-	require('connect-ensure-login').ensureLoggedIn(),
+	require('connect-ensure-login').ensureLoggedIn("/login"),
 	function(req, res){
 		res.render("faq");
 	})
