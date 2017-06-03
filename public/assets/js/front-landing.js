6a0ce2b053e460c14aee59b4726b8f074f313df3
meeshyd <michellemdidier@gmail.com>
2017-06-02 21:08:24 -0500

    added script to front end js to hide new user form until sign up link is pressed

--- public/assets/js/front-landing.js
+++ public/assets/js/front-landing.js
@@ -1,8 +1,54 @@
 $(document).ready(function(){
 
+	// hide new user sign in until sign-up link is clicked
+	$('#newuser-page').hide();
+	$('#sign-up-link').on('click',function(event){
+		$('#newuser-page').show();
+	})
+
 	// Materialize modal open/close
 	$('.modal').modal();
 
+	//ONE PAGE SMOOTH SCROLL//
+		
+		// Select all links with hashes
+		$('a[href*="#"]')
+		// Remove links that don't actually link to anything
+		.not('[href="#"]')
+		.not('[href="#0"]')
+		
+		.click(function(event) {
+		// On-page links
+		if (
+			location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+			&& 
+			location.hostname == this.hostname
+			) {
+			// Figure out element to scroll to
+			var target = $(this.hash);
+			console.log(this);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			// Does a scroll target exist?
+				if (target.length) {
+			 	// Only prevent default if animation is actually going to happen
+			        event.preventDefault();
+			        $('html, body').animate({
+			        scrollTop: target.offset().top
+			        }, 1000, function() {
+			            // Callback after animation
+			            // Must change focus!
+			            var $target = $(target);
+			            $target.focus();
+			            if ($target.is(":focus")) { // Checking if the target was focused
+			                // res.json(true);
+			            } else {
+			                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+			                $target.focus(); // Set focus again
+			            };
+			        });
+			    };
+			};
+		});
 
 	// The remaining code is for the Plaudit intro animation. 
 	// Note: Passport essentially refreshes page through a redirect upon incorrect password input,
@@ -31,46 +77,6 @@ $(document).ready(function(){
 		.to(["#plodet", "#definition"], .8, {opacity: 0, scale:1.5, ease:Back.easeOut}, "+=1")
 		.to(["#plodet", "#definition"], .8, {display: "none", scale:1.75, ease:Back.easeOut}, "-=.75")
 		.from("#sign-in", .6, {opacity: 0, scale:1.5, ease:Back.easeOut}, "+=0");
-		
-		//ONE PAGE SMOOTH SCROLL//
-		
-		// Select all links with hashes
-		$('a[href*="#"]')
-		// Remove links that don't actually link to anything
-		.not('[href="#"]')
-		.not('[href="#0"]')
-		
-		.click(function(event) {
-		// On-page links
-		if (
-			location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-			&& 
-			location.hostname == this.hostname
-			) {
-			// Figure out element to scroll to
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			// Does a scroll target exist?
-				if (target.length) {
-			 	// Only prevent default if animation is actually going to happen
-			        event.preventDefault();
-			        $('html, body').animate({
-			        scrollTop: target.offset().top
-			        }, 1000, function() {
-			            // Callback after animation
-			            // Must change focus!
-			            var $target = $(target);
-			            $target.focus();
-			            if ($target.is(":focus")) { // Checking if the target was focused
-			                // res.json(true);
-			            } else {
-			                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-			                $target.focus(); // Set focus again
-			            };
-			        });
-			    };
-			};
-		});
 
   	// If there is a login error, do not show/play the animation
 	}else{
